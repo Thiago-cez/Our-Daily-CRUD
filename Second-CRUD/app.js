04cdb7fd7a30aeec9ecb5bad9c9c6a4501492784
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-09-02 23:57:27 +0100

    second-Crud

---
+++ Second-CRUD/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app =express()
+app.use(express.json());
+
+const port = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor Rodando na porta ${port}`)
+})
