a4cfbde38853a443d0cc8813e2487c647e94562b
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-10-17 23:37:18 +0100

    commit

--- Fifth-Crud/index.js
+++ Fifth-Crud/index.js
@@ -1,12 +1,15 @@
 const express = require ("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-
 app.use(express.json())
+
+const connectionString = 
 
 
 app.listen( port , () => {
     console.log(`Servidor Rodando na porta ${port}`)
 })
+
